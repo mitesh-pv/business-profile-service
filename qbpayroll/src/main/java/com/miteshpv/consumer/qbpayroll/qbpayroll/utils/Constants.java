8384f387f1c07fa42673777cdfb193db6f034f72
Mitesh <mitesh.sit@gmail.com>
2021-12-29 00:24:30 +0530

    config changes

--- qbpayroll/src/main/java/com/miteshpv/consumer/qbpayroll/qbpayroll/utils/Constants.java
+++ qbpayroll/src/main/java/com/miteshpv/consumer/qbpayroll/qbpayroll/utils/Constants.java
@@ -1,7 +1,5 @@
 package com.miteshpv.consumer.qbpayroll.qbpayroll.utils;
 
 public interface Constants {
-    String BUSINESS_PROFILE_ROOT = "/api/business-profile";
-
     String PROTOCOL_SQS = "sqs";
 }
